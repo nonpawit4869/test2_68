c6664170febaf31f39052eab5db5c13d974fe055
013 นนท์ปวิธ สิทธิโชค! <n98728634@gmail.com>
2025-06-30 10:10:43 +0700

    main

---
+++ job3_10n72.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() 
+{
+    int num,num2;
+    printf("Enter number : ");
+    scanf("%f",&num);
+    num2=num*2;
+    if (num2==num*2)
+    printf("num2 = %.2f",num2);    
+}
